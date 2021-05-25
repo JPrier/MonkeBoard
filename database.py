f7fa1a705e90f0234c603c32f825a07a26689de3
Joshua Prier <JoshKPrier@gmail.com>
2021-05-24 20:14:52 -0700

    Add table check and creation

--- database.py
+++ database.py
@@ -3,28 +3,33 @@ import pymysql
 
 class database:
     def __init__(self, db):
-        self.db = db
         self.conn = pymysql.connect(
-            db=database_name,
+            db=db,
             user='root',
             passwd='',
             host='localhost')
         self.c = conn.cursor()
 
-    def add_row(args):
-        c.execute("INSERT INTO " + str(self.db) + " VALUES (" + ','.join(args) + ")")
+    def does_table_exist(table):
+        c.execute("SELECT TABLES LIKE '" + str(table) + "'")
+        return c.fetchone()
+
+    def add_table(table, columns):
+        c.execute("CREATE TABLE " + str(table) + str(columns))
+
+    def add_row(args, table):
+        c.execute("INSERT INTO " + str(table) + " VALUES (" + ','.join(args) + ")")
 
     def get_all():
-        c.execute("SELECT * FROM " + str(self.db))
+        c.execute("SELECT * FROM " + str(table))
         result = c.fetchall()
         for row in result:
             print(row, '\n')
 
     def get_values(args):
-        c.execute("SELECT " + str(args) + " FROM " + str(self.db))
+        c.execute("SELECT " + str(args) + " FROM " + str(table))
         print([row for row in c.fetchall()])
 
     def get_filtered(args, filter):
-        c.execute("SELECT " + str(args) + " FROM " + str(self.db) + " where " + str(filter))
+        c.execute("SELECT " + str(args) + " FROM " + str(table) + " where " + str(filter))
         print([row for row in c.fetchall()])
-        
